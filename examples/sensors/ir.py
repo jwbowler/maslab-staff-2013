bfb6a5461cc0d7e627b6818ef1f0c22e4ee9eb6b
MASLAB Team 3 <maslab-team3@maslab-team10.(none)>
2013-01-09 17:38:23 -0500

    Deleted a.out and update ir example

--- examples/sensors/ir.py
+++ examples/sensors/ir.py
@@ -11,11 +11,12 @@ import arduino
 THRESH = 200.0  # Experimentally chosen
 
 ard = arduino.Arduino()  # Create the Arduino object
-a0 = arduino.AnalogInput(ard, 0)  # Create an analog sensor on pin A0
+a1 = arduino.AnalogInput(ard, 1)
+a2 = arduino.AnalogInput(ard, 2)
+a3 = arduino.AnalogInput(ard, 5)
 ard.run()  # Start the thread which communicates with the Arduino
 
 # Main loop -- check the sensor and update the digital output
 while True:
-    ir_val = a0.getValue() # Note -- the higher value, the *closer* the dist
-    print ir_val, ir_val >= THRESH
+    print str(a1.getValue()) + " " + str(a2.getValue()) + " " + str(a3.getValue()) 
     time.sleep(0.1)
