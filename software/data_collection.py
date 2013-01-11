d238b7c712bee105721b1b536e525af8a0b843b1
jwbowler <jwbowler@gmail.com>
2013-01-11 18:07:49 -0500

    fixed ball distance calculation error

--- software/data_collection.py
+++ software/data_collection.py
@@ -12,7 +12,8 @@ class DataCollection:
     
         def getPolarFromScreen((x, y)):
             cam = self.cam
-            d = cam.imHeight * math.tan(cam.angle - cam.vfov/2 + cam.vfov*(y / cam.imHeight))
+            angle2obj = (cam.angle + cam.vfov/2 - cam.vfov*(1.0 * y / cam.imHeight))
+            d = cam.elev * math.tan((math.pi/180) * angle2obj)
             if d < 0:
                 d = math.isInf()
             a = (x - (cam.imHeight/2.)) * cam.hfov / cam.imWidth
@@ -28,8 +29,8 @@ class DataCollection:
         
 class Camera:
     def __init__(self):
-        self.elev = 1.
-        self.angle = 60. # 0 == pointing forward; 90 = pointing down
+        self.elev = 0.1
+        self.angle = 60. # 0 == pointing down; 90 = pointing forward
         self.imWidth = 640
         self.imHeight = 480
         self.ar = 1. * self.imWidth / self.imHeight
