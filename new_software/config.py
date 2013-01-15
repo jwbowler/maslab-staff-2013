85d1be15f6628544ef4361c82a01384b043669aa
Jorge Perez <jorgito@mit.edu>
2013-01-15 05:41:39 -0500

    Modified control.py and config.py

--- new_software/config.py
+++ new_software/config.py
@@ -1,4 +1,31 @@
-class Config:
+# game variables
+MY_BALLS_ARE_RED = True
 
-    MY_BALLS_ARE_RED = True
-    
+# Sensors
+IR_PINS = (0,0,0,0,0)
+# (distance from center, angle from front)
+IR_POSITIONS = ((0,0),(0,0),(0,0),(0,0),(0,0))
+ULTRASOUND_PINS =(0,0,0)
+MOTOR_CURRENT_PINS = (0,0,0,0)
+BALL_IN_SENSOR_PIN = 0
+BALL_OUT_SENSOR_PIN = 0
+
+# Motors (current, dir,pwn)
+ROLLER_PINS = (0,0,0)
+RIGHT_MOTOR_PINS = (0,0,0)
+LEFT_MOTOR_PINS = (0,0,0)
+HELIX_PINS = (0,0,0)
+RAMP_SERVO_PIN = 0
+SCORER_PIN=0
+
+
+# Control
+RIGHT_MOTOR_MIN = 8
+RIGHT_MOTOR_MAX = 127
+
+LEFT_MOTOR_MIN = 8
+LEFT_MOTOR_MAX = 127
+
+ROLLER_SPEED=0
+HELIX_SPEED=0
+
